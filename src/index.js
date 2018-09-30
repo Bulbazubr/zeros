4b831e421dfb7a44a65bc7fb99eea70dd881a8e7
Bulbazubr <hedin3299630@gmail.com>
2018-09-30 20:36:13 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+
+var counter = 0;
+    for (var i = number; i > 0; i = Math.floor(i/5)) {
+        counter= counter + Math.floor(i/5);
+    }
+    return counter;
 }
